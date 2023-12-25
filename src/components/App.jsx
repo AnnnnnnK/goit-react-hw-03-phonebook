805f251196989f0de9cc17ab4cc2bb41542a7496
AnnnnnnK <129075245+AnnnnnnK@users.noreply.github.com>
2023-12-25 11:42:03 +0100

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,31 @@ import { nanoid } from 'nanoid';
 import ContactsList from './ContactsList/ContactsList';
 import Filter from './Filter/Filter';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import data from '../components/contacts.json';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
-    name: '',
-    number: '',
   };
 
+  componentDidMount() {
+    const localData = localStorage.getItem('contacts');
+    if (localData && JSON.parse(localData).length > 0) {
+      this.setState({
+        contacts: JSON.parse(localData),
+      });
+    } else
+      this.setState({
+        contacts: data,
+      });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts?.length !== this.state.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   addNewContact = newContact => {
     const { contacts } = this.state;
 
@@ -67,12 +78,14 @@ class App extends Component {
       : contacts;
     return (
       <>
-        <ContactForm addNewContact={this.addNewContact}></ContactForm>
-        <Filter value={filter} onChange={this.changeFilter}></Filter>
-        <ContactsList
-          contacts={shownContacts}
-          handleDelete={this.onRemoveContact}
-        ></ContactsList>
+        <ContactForm addNewContact={this.addNewContact} />
+        <Filter value={filter} onChange={this.changeFilter} />
+        {this.state.contacts.length > 1 && (
+          <ContactsList
+            contacts={shownContacts}
+            handleDelete={this.onRemoveContact}
+          />
+        )}
       </>
     );
   }
